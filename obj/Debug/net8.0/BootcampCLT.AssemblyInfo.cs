26351f2e31e577be637eae6436a94f298a642220
Israel Arevalo <israelarevalo240@gmail.com>
2026-01-21 20:18:56 -0300

    ajustes

--- obj/Debug/net8.0/BootcampCLT.AssemblyInfo.cs
+++ obj/Debug/net8.0/BootcampCLT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BootcampCLT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d35793d4c23281e9ee41e34d36cfeeab353476ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("BootcampCLT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BootcampCLT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
